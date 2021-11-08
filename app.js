26f334c274fb36af1fe17a84520a97748bdbe21e
cschang07 <manutdsfan@gmail.com>
2021-11-08 18:27:04 +0800

    feat: 建立網站前台入口

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', handlebars())
+//Set handlebars as the view engine
+app.engine('handlebars', handlebars({ defaultLayout: 'main' })) //{ defaultLayout: 'main' } could be ignored since it has become default in handlebars v3.1.0 
 app.set('view engine', 'handlebars')
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+//1. Requires routes 2. Assigns "app" arg, so that the routes could use the object "app" to assign routes. 
+const router = require('./routes')
+router(app)
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
-module.exports = (app) => {
+const restController = require('../controllers/restController.js')
+module.exports = app => {
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+  //if user visits homepage, redirect to /restaurants
+  app.get('/', (req, res) => res.redirect('/restaurants'))
+
+  app.get('/restaurants', restController.getRestaurants)
 }
